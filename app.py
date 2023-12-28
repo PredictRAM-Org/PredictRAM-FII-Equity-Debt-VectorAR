b8ce6f5a86bc717adc8964000a426d195b824f85
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-28 15:24:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,53 +35,45 @@ equity_features = ["Equity Net Investment"]
 debt_features = ["Debt Net Investment"]
 target = "Percentage Change"
 
-# Split data into training and testing sets
-X_train_equity, X_test_equity, y_train, y_test = train_test_split(
-    merged_data[equity_features], merged_data[target], test_size=0.2, random_state=42
-)
+# Sidebar with date selector
+selected_date = st.sidebar.date_input("Select Date", min_value=merged_data["Date"].min(), max_value=merged_data["Date"].max())
 
-X_train_debt, X_test_debt, _, _ = train_test_split(
-    merged_data[debt_features], merged_data[target], test_size=0.2, random_state=42
-)
+# Filter data for the selected date
+selected_data = merged_data[merged_data["Date"] == selected_date]
 
-# Standardize features
-scaler_equity = StandardScaler()
-X_train_scaled_equity = scaler_equity.fit_transform(X_train_equity)
-X_test_scaled_equity = scaler_equity.transform(X_test_equity)
-
-scaler_debt = StandardScaler()
-X_train_scaled_debt = scaler_debt.fit_transform(X_train_debt)
-X_test_scaled_debt = scaler_debt.transform(X_test_debt)
+# Display selected data
+st.write("Selected Data:")
+st.write(selected_data)
 
 # Linear Regression model for Equity
 linear_model_equity = LinearRegression()
-linear_model_equity.fit(X_train_scaled_equity, y_train)
-linear_pred_equity = linear_model_equity.predict(X_test_scaled_equity)
+linear_model_equity.fit(merged_data[equity_features], merged_data[target])
+linear_pred_equity = linear_model_equity.predict(selected_data[equity_features])
 
 # Linear Regression model for Debt
 linear_model_debt = LinearRegression()
-linear_model_debt.fit(X_train_scaled_debt, y_train)
-linear_pred_debt = linear_model_debt.predict(X_test_scaled_debt)
+linear_model_debt.fit(merged_data[debt_features], merged_data[target])
+linear_pred_debt = linear_model_debt.predict(selected_data[debt_features])
 
 # Random Forest Regression model for Equity
 rf_model_equity = RandomForestRegressor()
-rf_model_equity.fit(X_train_scaled_equity, y_train)
-rf_pred_equity = rf_model_equity.predict(X_test_scaled_equity)
+rf_model_equity.fit(merged_data[equity_features], merged_data[target])
+rf_pred_equity = rf_model_equity.predict(selected_data[equity_features])
 
 # Random Forest Regression model for Debt
 rf_model_debt = RandomForestRegressor()
-rf_model_debt.fit(X_train_scaled_debt, y_train)
-rf_pred_debt = rf_model_debt.predict(X_test_scaled_debt)
+rf_model_debt.fit(merged_data[debt_features], merged_data[target])
+rf_pred_debt = rf_model_debt.predict(selected_data[debt_features])
 
 # Gradient Boosting Regression model for Equity
 gb_model_equity = GradientBoostingRegressor()
-gb_model_equity.fit(X_train_scaled_equity, y_train)
-gb_pred_equity = gb_model_equity.predict(X_test_scaled_equity)
+gb_model_equity.fit(merged_data[equity_features], merged_data[target])
+gb_pred_equity = gb_model_equity.predict(selected_data[equity_features])
 
 # Gradient Boosting Regression model for Debt
 gb_model_debt = GradientBoostingRegressor()
-gb_model_debt.fit(X_train_scaled_debt, y_train)
-gb_pred_debt = gb_model_debt.predict(X_test_scaled_debt)
+gb_model_debt.fit(merged_data[debt_features], merged_data[target])
+gb_pred_debt = gb_model_debt.predict(selected_data[debt_features])
 
 # LSTM model for Equity
 def create_lstm_model(input_shape):
@@ -91,75 +83,61 @@ def create_lstm_model(input_shape):
     model.compile(optimizer="adam", loss="mse")
     return model
 
-X_train_lstm_equity = np.reshape(X_train_scaled_equity, (X_train_scaled_equity.shape[0], X_train_scaled_equity.shape[1], 1))
-X_test_lstm_equity = np.reshape(X_test_scaled_equity, (X_test_scaled_equity.shape[0], X_test_scaled_equity.shape[1], 1))
+X_train_lstm_equity = np.reshape(merged_data[equity_features], (merged_data.shape[0], len(equity_features), 1))
+X_test_lstm_equity = np.reshape(selected_data[equity_features], (1, len(equity_features), 1))
 
-lstm_model_equity = create_lstm_model(X_train_scaled_equity.shape[1])
-lstm_model_equity.fit(X_train_lstm_equity, y_train, epochs=50, batch_size=32, verbose=0)
+lstm_model_equity = create_lstm_model(len(equity_features))
+lstm_model_equity.fit(X_train_lstm_equity, merged_data[target], epochs=50, batch_size=32, verbose=0)
 lstm_pred_equity = lstm_model_equity.predict(X_test_lstm_equity).flatten()
 
 # LSTM model for Debt
-X_train_lstm_debt = np.reshape(X_train_scaled_debt, (X_train_scaled_debt.shape[0], X_train_scaled_debt.shape[1], 1))
-X_test_lstm_debt = np.reshape(X_test_scaled_debt, (X_test_scaled_debt.shape[0], X_test_scaled_debt.shape[1], 1))
+X_train_lstm_debt = np.reshape(merged_data[debt_features], (merged_data.shape[0], len(debt_features), 1))
+X_test_lstm_debt = np.reshape(selected_data[debt_features], (1, len(debt_features), 1))
 
-lstm_model_debt = create_lstm_model(X_train_scaled_debt.shape[1])
-lstm_model_debt.fit(X_train_lstm_debt, y_train, epochs=50, batch_size=32, verbose=0)
+lstm_model_debt = create_lstm_model(len(debt_features))
+lstm_model_debt.fit(X_train_lstm_debt, merged_data[target], epochs=50, batch_size=32, verbose=0)
 lstm_pred_debt = lstm_model_debt.predict(X_test_lstm_debt).flatten()
 
 # ARIMA model for Equity
 arima_model_equity = ARIMA(merged_data[target], order=(5, 1, 0))
 arima_result_equity = arima_model_equity.fit()
-arima_pred_equity = arima_result_equity.predict(start=len(merged_data), end=len(merged_data) + len(X_test_equity) - 1, typ='levels')
+arima_pred_equity = arima_result_equity.predict(start=len(merged_data), end=len(merged_data) + 1, typ='levels')
 
 # ARIMA model for Debt
 arima_model_debt = ARIMA(merged_data[target], order=(5, 1, 0))
 arima_result_debt = arima_model_debt.fit()
-arima_pred_debt = arima_result_debt.predict(start=len(merged_data), end=len(merged_data) + len(X_test_debt) - 1, typ='levels')
+arima_pred_debt = arima_result_debt.predict(start=len(merged_data), end=len(merged_data) + 1, typ='levels')
 
 # VAR model for Equity
 var_model_equity = VAR(merged_data[["Percentage Change", "Equity Net Investment"]])
 var_result_equity = var_model_equity.fit()
-var_pred_equity = var_result_equity.forecast(merged_data[["Percentage Change", "Equity Net Investment"]].values, steps=len(X_test_equity))
+var_pred_equity = var_result_equity.forecast(merged_data[["Percentage Change", "Equity Net Investment"]].values, steps=1)
 
 # VAR model for Debt
 var_model_debt = VAR(merged_data[["Percentage Change", "Debt Net Investment"]])
 var_result_debt = var_model_debt.fit()
-var_pred_debt = var_result_debt.forecast(merged_data[["Percentage Change", "Debt Net Investment"]].values, steps=len(X_test_debt))
+var_pred_debt = var_result_debt.forecast(merged_data[["Percentage Change", "Debt Net Investment"]].values, steps=1)
 
-# Plotting for Equity and NIFTY (Date-wise)
-st.title("Comparison of Equity Predictions with NIFTY Percentage Change (Date-wise)")
+# Plotting
+st.title("Comparison of Predictions with NIFTY Percentage Change for Selected Date")
+
 plt.figure(figsize=(10, 6))
-for i in range(len(y_test)):
-    date = y_test.index[i]
-    
-    plt.plot([date, date], [y_test.iloc[i], linear_pred_equity[i]], label="Linear Regression")
-    plt.plot([date, date], [y_test.iloc[i], rf_pred_equity[i]], label="Random Forest")
-    plt.plot([date, date], [y_test.iloc[i], gb_pred_equity[i]], label="Gradient Boosting")
-    plt.plot([date, date], [y_test.iloc[i], lstm_pred_equity[i]], label="LSTM")
-    plt.plot([date, date], [y_test.iloc[i], arima_pred_equity.iloc[i]], label="ARIMA")
-    plt.plot([date, date], [y_test.iloc[i], var_pred_equity[i, 0]], label="VAR")
 
-plt.title("Equity Predictions vs. NIFTY Percentage Change (Date-wise)")
-plt.xlabel("Date")
-plt.ylabel("NIFTY Percentage Change")
-plt.legend()
-st.pyplot(plt)
+# Plotting for Equity
+plt.plot(["Linear Regression", "Random Forest", "Gradient Boosting", "LSTM", "ARIMA", "VAR"],
+         [linear_pred_equity[0], rf_pred_equity[0], gb_pred_equity[0], lstm_pred_equity[0], arima_pred_equity.iloc[0], var_pred_equity[0, 0]],
+         label="Equity Models")
 
-# Plotting for Debt and NIFTY (Date-wise)
-st.title("Comparison of Debt Predictions with NIFTY Percentage Change (Date-wise)")
-plt.figure(figsize=(10, 6))
-for i in range(len(y_test)):
-    date = y_test.index[i]
-    
-    plt.plot([date, date], [y_test.iloc[i], linear_pred_debt[i]], label="Linear Regression")
-    plt.plot([date, date], [y_test.iloc[i], rf_pred_debt[i]], label="Random Forest")
-    plt.plot([date, date], [y_test.iloc[i], gb_pred_debt[i]], label="Gradient Boosting")
-    plt.plot([date, date], [y_test.iloc[i], lstm_pred_debt[i]], label="LSTM")
-    plt.plot([date, date], [y_test.iloc[i], arima_pred_debt.iloc[i]], label="ARIMA")
-    plt.plot([date, date], [y_test.iloc[i], var_pred_debt[i, 0]], label="VAR")
+# Plotting for Debt
+plt.plot(["Linear Regression", "Random Forest", "Gradient Boosting", "LSTM", "ARIMA", "VAR"],
+         [linear_pred_debt[0], rf_pred_debt[0], gb_pred_debt[0], lstm_pred_debt[0], arima_pred_debt.iloc[0], var_pred_debt[0, 0]],
+         label="Debt Models")
 
-plt.title("Debt Predictions vs. NIFTY Percentage Change (Date-wise)")
-plt.xlabel("Date")
+# Plotting for Actual NIFTY Percentage Change
+plt.axhline(y=selected_data[target].values[0], color='r', linestyle='--', label="Actual NIFTY Percentage Change")
+
+plt.title(f"Comparison for Date: {selected_date}")
+plt.xlabel("Models")
 plt.ylabel("NIFTY Percentage Change")
 plt.legend()
 st.pyplot(plt)
